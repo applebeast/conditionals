4a3f9469734f920fa222858666a8975be4bfe9e6
applebeast <Earl@Earls-iMac.local>
2016-07-29 08:42:19 -0400

    first commit

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+var my_house_is_burning: Bool = false
+
+var anotherbool = true
+
+
+
+if my_house_is_burning {
+print ("I'm gonna get a lot of insurance money!!!!!!!")
+
+} else {
+
+print ("I'm gonna need to get money a different way...")
+}
+
+var result = false == false
+
+var enoughmoney = 500
+
+var walletmoney = 500
+
+if enoughmoney <= walletmoney {
+print ("yay. You can buy an iphone SE, or 500 candy bars")
+}
+else {
+    print ("you are too poor to buy an iphone SE like me. You suck.")
+}
+
+var finger = "ring"
+var fing1 = "index"
+var fing2 = "pinky"
+var fing3 = "ring"
+var fing4 = "middle fucker"
+if finger == fing2 {
+    print ("you guessed my finger!!!")
+    
+} else if finger == fing1 {
+        print ("nope. The finger I'm holding up is not my index finger.")
+
+} else if finger == fing3 {
+        print ("nope. The finger I'm holding up ain't my ring finger.")
+
+} else if finger == fing4 {
+    print ("no, I'm not flipping you off")
+    
+} else {
+print ("no, please guess between ring, middle fucker, pinky, and index")
+}
