9f92e342ec30e4ba3b1c9f5e3bbb04d9725cf20a
applebeast <Earl@Earls-iMac.local>
2016-07-29 09:20:43 -0400

    added conditional 2 playground

---
+++ conditional2.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+import UIKit
+
+var myaccount = 1000.00
+
+var myfriendsaccount = 2000.00
+
+var myfriendsfriendsaccount = 3000.00
+
+if myaccount > 900.00 && myfriendsaccount > 1500.00 && myfriendsfriendsaccount > 2000.00 {
+    print ("we've got fucking money!!!!")
+}
+var playerAAlive = true
+var playerBAlive = true
+var playerCAlive = true
+
+if playerAAlive == false || playerBAlive == false || playerCAlive == false {
+    print ("one of your team members died")
+}
+
+//can I retire???
+var age = 14
+var account = 10000
+var grampa = false
+
+if account > 7000 && age >= 60 || grampa == true {
+    print ("I can retire")
+}
+else {
+    print ("I cant retire")
+}
